cb4f5d06115f2a34416f884914fb349aafafa603
teamcity <teamcity@m1.local>
2021-06-21 15:06:14 +0100

    Publishing javadoc for chronicle-threads:2.21ea64-SNAPSHOT

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"net.openhft.chronicle.threads"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.openhft.chronicle.threads"},{"l":"net.openhft.chronicle.threads.internal"}]
